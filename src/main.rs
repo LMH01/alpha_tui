72345167026ca9cf2d61a9729f27cf0bef839b78
LMH01 <louis.haensch01@gmail.com>
2023-04-06 17:43:01 +0200

    Add new way to handle registers (not compiling)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
 fn main() {
     println!("Hello, world!");
     
-    let mut instructions = Vec::new();
-    instructions.push(Instruction::PushStack(2));
-    instructions.push(Instruction::PopStack());
-    instructions.push(Instruction::PrintRegister());
+    let instructions = vec![Instruction::PushStack(2),
+        Instruction::PopStack(),
+        Instruction::PrintRegister()];
     let mut runner = Runner::new(instructions);
     runner.run();
+}
+
+/// All registers that can contain data
+enum RegisterLabel {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+}
+
+/// Representation of a single register, containing data 
+struct Register {
+    data: Option<i32>,
+    label: RegisterLabel,
+}
+
+impl Register {
+    /// Creates a new register
+    fn new(label: RegisterLabel) -> Self {
+        Self {
+            data: None,
+            label,
+        }
+    }
 }
 
 struct Runner {
@@ -30,14 +55,22 @@ impl Runner {
 }
 
 struct RuntimeArgs {
-    register: i32,
+    registers: Vec<Register>,
     stack: Vec<i32>,
 }
 
 impl RuntimeArgs {
     fn new() -> Self {
+        let registers = vec![
+            Register::new(RegisterLabel::A),
+            Register::new(RegisterLabel::B),
+            Register::new(RegisterLabel::C),
+            Register::new(RegisterLabel::D),
+            Register::new(RegisterLabel::E),
+            Register::new(RegisterLabel::F),
+        ];
         Self {
-            register: 0,
+            registers,
             stack: Vec::new(),
         }
     }
