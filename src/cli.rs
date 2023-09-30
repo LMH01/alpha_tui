LMH01/alpha_tui

commit f55249eb9da9a4d4003582fa7fea1f360679e0f8

Changed option named disable_pretty_print to disable_alignment

src/cli.rs
@@ -60,7 +60,7 @@ pub struct Args {
         help = "Disable alignment of labels, instructions and comments",
         long_help = "Per default labels, instructions and comments are aligned in columns to make reading easier, this can be disabled by setting this flag."
     )]
-    pub disable_pretty_print: bool,
+    pub disable_alignment: bool,
     #[arg(
         short,
         long,

src/main.rs
@@ -51,7 +51,7 @@ fn main() -> Result<()> {
     )?;
 
     // format instructions pretty if cli flag is set
-    let instructions = match args.disable_pretty_print {
+    let instructions = match args.disable_alignment {
         false => pretty_format_instructions(&instructions),
         true => instructions,
     };

commit 15eff1069770d93fa0acfec895152fed029feec7

Fixed crash introduced in f55249eb9da9a4d4003582fa7fea1f360679e0f8

src/cli.rs
@@ -66,7 +66,7 @@ pub struct Args {
         long,
         help = "Write the changed program file alignment to file",
         long_help = "Write the changed program file alignment for better readability to the source file.",
-        conflicts_with = "disable_pretty_print",
+        conflicts_with = "disable_alignment",
     )]
     pub write_alignment: bool
 }