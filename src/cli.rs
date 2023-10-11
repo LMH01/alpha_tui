449db4c12dd333e04f67d1e68a4fefdf106b3992
LMH01 <louis.haensch01@gmail.com>
2023-10-11 21:48:50 +0200

    Moved one test #15

--- src/cli.rs
+++ src/cli.rs
@@ -163,10 +163,10 @@ mod tests {
         let mut cmd = Command::cargo_bin("alpha_tui").unwrap();
         let assert = cmd
             .arg("check")
-            .arg("tests/test_cmd_check_compile_with_allowed_instructions/program.alpha")
+            .arg("tests/test_cmd_check_compile_with_allowed_instructions_2/program.alpha")
             .arg("compile")
             .arg("--allowed-instructions")
-            .arg("tests/test_cmd_check_compile_with_allowed_instructions/instructions2.txt")
+            .arg("tests/test_cmd_check_compile_with_allowed_instructions_2/instructions.txt")
             .assert();
         assert.success();
     }
