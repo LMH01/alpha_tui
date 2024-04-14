fcd87481fdbc4e215a736dff7fb20b1cb255fd6d
LMH01 <louis.haensch01@gmail.com>
2024-04-15 00:38:51 +0200

    Added windows cross compilation to nix flake

--- flake.nix
+++ flake.nix
@@ -4,8 +4,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    crane.url = "github:ipetkov/crane";
-    crane.inputs.nixpkgs.follows = "nixpkgs";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs:
@@ -36,6 +42,15 @@
               # disable check because two tests fail because files can not be found (needs to be fixed, but I currently don't know how)
               doCheck = false;
             };
+
+            # cross compilation to windows
+            toolchainWin = with inputs.fenix.packages.${system};
+              combine [
+                minimal.rustc
+                minimal.cargo
+                targets.x86_64-pc-windows-gnu.latest.rust-std
+              ];
+            craneLibWin = (inputs.crane.mkLib pkgs).overrideToolchain toolchainWin;
           in
           {
             devShells.default = pkgs.mkShell {
@@ -52,6 +67,25 @@
 
               default = alpha_tui;
 
+              alpha_tui-win = craneLibWin.buildPackage {
+                src = src;
+
+                strictDepts = true;
+                doCheck = false;
+
+                CARGO_BUILD_TARGET = "x86_64-pc-windows-gnu";
+                RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
+                  pkgs.pkgsCross.mingwW64.windows.pthreads
+                ]);
+
+                depsBuildBuild = with pkgs;
+                  [
+                    pkgsCross.mingwW64.stdenv.cc
+                    pkgsCross.mingw32.windows.pthreads
+                    # somehow the build fails if this 64 bit version is used instead of the 32 bit version
+                    #pkgsCross.mingwW64.windows.pthreads
+                  ];
+              };
             };
 
           };
