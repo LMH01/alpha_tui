4318f7130895ea697dd73f674205db260e133ba2
LMH01 <louis.haensch01@gmail.com>
2024-05-06 19:27:45 +0200

    Reorderd some tests

---
+++ tests/allowed_instructions.rs
@@ -0,0 +1,31 @@
+use assert_cmd::Command;
+
+#[test]
+fn test_allowed_instructions_file() {
+    let mut cmd = Command::cargo_bin("alpha_tui").unwrap();
+    let assert = cmd
+        .arg("check")
+        .arg("tests/input/test_allowed_instructions_file_program.alpha")
+        .arg("compile")
+        .arg("--allowed-instructions-file")
+        .arg("examples/allowed_instructions.json")
+        .assert();
+    assert.success();
+}
+
+#[test]
+fn test_allowed_instructions_file_full() {
+    let mut cmd = Command::cargo_bin("alpha_tui").unwrap();
+    let assert = cmd
+        .arg("check")
+        .arg("tests/input/test_allowed_instructions_file_full/program.alpha")
+        .arg("compile")
+        .arg("--allowed-instructions-file")
+        .arg("tests/input/test_allowed_instructions_file_full/instructions.json")
+        .arg("--allowed-operations")
+        .arg("add,sub")
+        .arg("--allowed-comparisons")
+        .arg("lt,le,eq")
+        .assert();
+    assert.success();
+}
--- tests/cli.rs
+++ tests/cli.rs
@@ -25,20 +25,3 @@ fn test_cmd_check_compile_with_allowed_instructions_2() {
         .assert();
     assert.success();
 }
-
-#[test]
-fn test_cmd_check_compile_with_allowed_instructions_comparisons_operations() {
-    let mut cmd = Command::cargo_bin("alpha_tui").unwrap();
-    let assert = cmd
-        .arg("check")
-        .arg("tests/input/test_cmd_check_compile_with_allowed_instructions_comparisons_operations/program.alpha")
-        .arg("compile")
-        .arg("--allowed-instructions-file")
-        .arg("tests/input/test_cmd_check_compile_with_allowed_instructions_comparisons_operations/instructions.json")
-        .arg("--allowed-operations")
-        .arg("add,sub")
-        .arg("--allowed-comparisons")
-        .arg("lt,le,eq")
-        .assert();
-    assert.success();
-}
