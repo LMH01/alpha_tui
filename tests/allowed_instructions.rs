3936d0fe6ce9564010f8c8fbadeb86f5eedf3900
LMH01 <louis.haensch01@gmail.com>
2024-05-06 20:06:32 +0200

    Added more tests for #72

--- tests/allowed_instructions.rs
+++ tests/allowed_instructions.rs
@@ -42,3 +42,122 @@ fn test_allowed_instructions_only_instructions() {
         .assert();
     assert.success();
 }
+
+#[test]
+fn test_allowed_instructions_only_instructions_fail() {
+    let mut cmd = Command::cargo_bin("alpha_tui").unwrap();
+    let assert = cmd
+        .arg("check")
+        .arg("tests/input/test_allowed_instructions_only_instructions_fail/program.alpha")
+        .arg("compile")
+        .arg("--allowed-instructions-file")
+        .arg("tests/input/test_allowed_instructions_only_instructions_fail/instructions.json")
+        .assert();
+    assert.stdout(
+        r#"Building instructions
+Building runtime
+Check unsuccessful, program did not compile.
+Error: build_program_error
+
+  × when building program
+  ╰─▶ build_program::instruction_not_allowed_error
+      
+        × instruction 'p(h1) := 20' in line '2' is not allowed
+        help: Make sure that you include this type ('M := C') of instruction
+      in
+              the whitelist or use a different instruction.
+              These types of instructions are allowed:
+      
+              A := C
+      
+
+"#,
+    );
+}
+
+#[test]
+fn test_allowed_instructions_only_comparisons() {
+    let mut cmd = Command::cargo_bin("alpha_tui").unwrap();
+    let assert = cmd
+        .arg("check")
+        .arg("tests/input/test_allowed_instructions_only_comparisons/program.alpha")
+        .arg("compile")
+        .arg("--allowed-instructions-file")
+        .arg("tests/input/test_allowed_instructions_only_comparisons/instructions.json")
+        .assert();
+    assert.success();
+}
+
+#[test]
+fn test_allowed_instructions_only_comparisons_fail() {
+    let mut cmd = Command::cargo_bin("alpha_tui").unwrap();
+    let assert = cmd
+        .arg("check")
+        .arg("tests/input/test_allowed_instructions_only_comparisons_fail/program.alpha")
+        .arg("compile")
+        .arg("--allowed-instructions-file")
+        .arg("tests/input/test_allowed_instructions_only_comparisons_fail/instructions.json")
+        .assert();
+    assert.stdout(
+            r#"Building instructions
+Building runtime
+Check unsuccessful, program did not compile.
+Error: build_program_error
+
+  × when building program
+  ╰─▶ build_program::comparison_not_allowed_error
+      
+        × comparison '==' in line '1' is not allowed
+        help: Make sure that you include this comparison ('==') in the allowed
+              comparisons or use a different instruction.
+              To mark this comparison as allowed you can use: '--allowed-
+              comparisons "eq"'
+      
+
+"#
+    );
+}
+
+#[test]
+fn test_allowed_instructions_only_operations() {
+    let mut cmd = Command::cargo_bin("alpha_tui").unwrap();
+    let assert = cmd
+        .arg("check")
+        .arg("tests/input/test_allowed_instructions_only_operations/program.alpha")
+        .arg("compile")
+        .arg("--allowed-instructions-file")
+        .arg("tests/input/test_allowed_instructions_only_operations/instructions.json")
+        .assert();
+    assert.success();
+}
+
+#[test]
+fn test_allowed_instructions_only_operations_fail() {
+    let mut cmd = Command::cargo_bin("alpha_tui").unwrap();
+    let assert = cmd
+        .arg("check")
+        .arg("tests/input/test_allowed_instructions_only_operations_fail/program.alpha")
+        .arg("compile")
+        .arg("--allowed-instructions-file")
+        .arg("tests/input/test_allowed_instructions_only_operations_fail/instructions.json")
+        .assert();
+    assert.stdout(
+            r#"Building instructions
+Building runtime
+Check unsuccessful, program did not compile.
+Error: build_program_error
+
+  × when building program
+  ╰─▶ build_program::operation_not_allowed_error
+      
+        × operation '-' in line '2' is not allowed
+        help: Make sure that you include this operation ('-') in the allowed
+              operations or use a different instruction.
+              To mark this operation as allowed you can use: '--allowed-
+      operations
+              "sub"'
+      
+
+"#
+    );
+}
